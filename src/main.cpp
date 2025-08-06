e9bc86c917c36ce8a29b8341304225f97869ae74
coller95 <hongyeoh95@gmail.com>
2025-08-06 22:25:09 +0800

    Refactor main function into Application class for better structure and encapsulation

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
-#include "printHello.h"
-#include "foo.h"
-#include "foo_c.h"
+#include <printHello.h>
+#include <foo.h>
+#include <foo_c.h>
 
-int main() {
-    printHello();
-    external_print("Hello from the external C++ library!");
-    external_print_c("Hello from the external C library!");
-    return 0;
+/**
+ * Application class encapsulates the main application logic
+ */
+class Application
+{
+  public:
+	/**
+	 * Runs the application
+	 * @return Exit code
+	 */
+	int run()
+	{
+		printHello();
+		external_print("Hello from the external C++ library!");
+		external_print_c("Hello from the external C library!");
+		return 0;
+	}
+};
+
+int main()
+{
+	Application app;
+	return app.run();
 }
